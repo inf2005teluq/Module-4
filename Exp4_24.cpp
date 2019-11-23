6e84ce54a63b373b6ffe93af67e8184b306a5ecd
INF2005 <57367262+inf2005teluq@users.noreply.github.com>
2019-11-23 16:10:21 -0500

    Create Exp4_24.cpp

---
+++ Exp4_24.cpp
@@ -0,0 +1,10 @@
+#include<iostream> 
+// sans le .h si Linux
+using namespace std;
+void main(){int Age ; 
+/*déclaration de vos variables*/
+cout << "Bonjour \n" << " donnez votre âge : " ;
+cin >> Age ;
+cout << "Votre âge est de " << Age << " ans\n" ;
+    
+}
